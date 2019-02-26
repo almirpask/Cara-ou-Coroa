7c3a60190dfd266c52a21a04860937176869a696
almirpask <almirpask@hotmail.com>
2019-02-25 23:22:29 -0300

    Initial Commit

---
+++ Cara-ou-Coroa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cara-ou-Coroa
+//
+//  Created by Almir Santos on 25/02/19.
+//  Copyright © 2019 Almir Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
